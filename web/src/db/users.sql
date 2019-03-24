d7cea22d3ebf32a905788ed7a1a694b89d5547ad
Kyujin Cho <thy2134@gmail.com>
2019-03-24 16:49:47 +0900

    Add server code

--- web/src/db/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    name TEXT NOT NULL,
-    phone TEXT, 
-    email TEXT,
-    status BOOLEAN DEFAULT false
-);
